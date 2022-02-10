e4578feaa078580c2766b381eed3b4196c6a505d
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-10 09:28:37 +0530

    Add files via upload

---
+++ File_Handling/Filestream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practice
+{
+    class Filestream
+    {
+        public void display()
+        {
+            FileStream fs = new FileStream("C:\\Users\\ELCOT\\Desktop\\FS.txt", FileMode.OpenOrCreate);
+            fs.WriteByte(89);
+            fs.Close();
+        }
+        
+    }
+}
---
+++ File_Handling/SW_SR.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practice
+{
+    class SW_SR
+    {
+        public void display1()
+        {
+            FileStream f = new FileStream("C:\\Users\\ELCOT\\Desktop\\SW_SR.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(f);
+
+            sw.WriteLine("Hello");
+            sw.Close();
+            f.Close();
+        }
+
+        public void display2()
+        {
+            FileStream FS = new FileStream("C:\\Users\\ELCOT\\Desktop\\SW_SR.txt", FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(FS);
+
+            string value = sr.ReadLine();
+            Console.WriteLine("Good Morning..");
+            sr.Close();
+            sr.Close();
+        }
+    }
+}
