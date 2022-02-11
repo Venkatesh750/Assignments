c05b850ecc40bcdd798667f73f18f5da6b8c8aa4
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 09:56:55 +0530

    Add files via upload

---
+++ Background_Thread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Types_of_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(fn1);
+            th.IsBackground = true;
+            th.Start();
+            Console.WriteLine("Main method ends");
+        }
+
+        public static void fn1()
+        {
+            Console.WriteLine("Function 1");
+            Thread.Sleep(4000);
+            Console.WriteLine("Function ends");
+        }
+    }
+    
+}
