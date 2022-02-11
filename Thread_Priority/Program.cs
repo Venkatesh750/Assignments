759c512da2822754f34265f6e0a461e8dd7dbfb1
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 09:52:06 +0530

    Add files via upload

---
+++ Thread_Priority/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread_Priority
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(Display1);
+            Thread th2 = new Thread(Display2);
+            Thread th3 = new Thread(Display3);
+
+            th1.Priority = ThreadPriority.Normal;
+            th2.Priority = ThreadPriority.Lowest;
+            th3.Priority = ThreadPriority.Highest;
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+
+        }
+        public static void Display1()
+        {
+            Console.WriteLine("This is Display1 method");
+        }
+
+        public static void Display2()
+        {
+            Console.WriteLine("This is Display2 method");
+        }
+
+        public static void Display3()
+        {
+            Console.WriteLine("This is Display3 method");
+        }
+    }
+}
