8a0d2726bb6e7fe70410e0be271a2e221d1c4936
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 10:22:17 +0530

    Update Program.cs

--- Background_Thread/Program.cs
+++ Background_Thread/Program.cs
@@ -11,12 +11,18 @@ namespace Types_of_Thread
     {
         static void Main(string[] args)
         {
+            // create an instance of thread
             Thread th = new Thread(fn1);
+            
+            // set the thread as in background
             th.IsBackground = true;
             th.Start();
+            
+            // main method end
             Console.WriteLine("Main method ends");
         }
-
+           
+        // creating a method fn1
         public static void fn1()
         {
             Console.WriteLine("Function 1");
