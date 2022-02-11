0e59b5ae95b9d7f42d4c39efae18fb9914d2bf60
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 12:27:43 +0530

    Add files via upload

---
+++ Interface/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface Interface1
+    {
+        // Interface is a blueprint of a class
+
+        // creating a method without body and it is only a structure  
+        void fn1();
+        void fn2();
+    }
+}
