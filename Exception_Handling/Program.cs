ed45065cc506dc98f374ea495dea955f11594a3c
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 10:50:24 +0530

    Add files via upload

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initializing the value for x
+            int x = 0;
+
+            // creating a variable y as integer
+            int y;
+
+            // Exception Handling
+
+            // try block holds the code that may throw an exception
+            try
+            {
+                y = 10 / x;
+            }
+
+            // catch block catches the exception thrown by the try block
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exception occured");
+            }
+
+            // This block holds the default code
+            finally
+            {
+                Console.WriteLine("Finally Block");
+            }
+        }
+    }
+}
