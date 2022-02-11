fc59e5252c252d50b0818c5c266e5804aa077b2a
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 11:55:06 +0530

    Add files via upload

---
+++ Collections/Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stack stores elements in LIFO style (Last In First Out)
+
+            // calling the method
+            stk();
+        }
+
+        public static void stk()
+        {
+            // creating a stack
+            Stack sk = new Stack();
+
+            // pushing the value into the stack (add)
+            sk.Push("Venkatesh");
+            sk.Push("I\'m");
+            sk.Push("Hello!!");
+
+            // popping the value from the stack (remove)
+            sk.Pop();
+
+            // print the values from the stack
+            foreach(object val in sk)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
