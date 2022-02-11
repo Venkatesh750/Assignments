aad4300d31512e8c1dae1a359aab4f5213194505
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-11 12:26:08 +0530

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating an object
+            Interface1 obj = new demo();
+
+            // calling the two methods
+            obj.fn1();
+            obj.fn2();
+
+        }
+    }
+}
