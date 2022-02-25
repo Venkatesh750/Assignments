ef4f3cd958b24d440efee442c2fc5d8e83672030
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-02-25 11:46:55 +0530

    Add files via upload

---
+++ FileInfo and Serializable/File_Info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practice1
+{
+    public class File_Info
+    {
+        
+       public void Fun()
+        {
+            try
+            {
+                string path = "C:\\Users\\ELCOT\\Desktop\\sample.txt";
+                FileInfo obj = new FileInfo(path);
+                StreamWriter s1 = obj.CreateText();
+
+                Console.WriteLine("Enter the text ..");
+                string val = Console.ReadLine();
+                s1.WriteLine(val);
+                Console.WriteLine(s1);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error is : "+e); 
+            }
+        }
+    }
+}
---
+++ FileInfo and Serializable/demo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Practice1
+{
+    public class demo
+    {
+        public void Fn()
+        {
+            FileStream f = new FileStream("C:\\Users\\ELCOT\\Desktop\\sample1.txt",FileMode.OpenOrCreate);
+            BinaryFormatter b = new BinaryFormatter();
+
+            Console.WriteLine("Enter your name : ");
+            string value = Console.ReadLine();
+
+            Console.WriteLine("Enter your acc no. ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your bal : ");
+            int balance = int.Parse(Console.ReadLine());
+
+            Se s = new Se(value, a, balance); 
+            b.Serialize(f, s);
+            f.Close();
+        }
+
+        public void Fn1()
+        {
+            FileStream fs = new FileStream("C:\\Users\\ELCOT\\Desktop\\sample1.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            Se s = (Se)bf.Deserialize(fs);
+            Console.WriteLine("Your name : {0} \nYour acc no. {1} \nYour acc bal : {2}",s.a_name,s.a_no,s.bal);
+            fs.Close();
+        }
+    }
+}
