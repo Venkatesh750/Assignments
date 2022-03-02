16b2c94a3447394855955f7993185ece2780bf58
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-03-02 12:11:40 +0530

    Update Employee_DB_Assignments.sql

--- SQL_Assignment(Employee_DB)/Employee_DB_Assignments.sql
+++ SQL_Assignment(Employee_DB)/Employee_DB_Assignments.sql
@@ -2,26 +2,29 @@ create database ORG;
 use ORG;
 create table Employee(Employee_ID int not null primary key identity(1,1), First_Name varchar(25), Last_Name varchar(25), Salary int, Joining_Date varchar(50), Department varchar(25));
 select * from Employee;
-insert into Employee(First_Name, Last_Name, Salary, Joining_Date, Department) values ('Monika', 'Arora', 100000, '14ñ02ñ20 09.00.00',  'HR'),('Niharika', 'Verma', 80000, '14ñ06ñ11 09.00.00',  'Admin'), ('Vishal', 'Singhal', 300000, '14ñ02ñ20 09.00.00',  'HR'), ('Amitabh', 'Singh', 500000, '14ñ02ñ20 09.00.00',  'Admin'), ('Vivek', 'Bhati', 500000, '14ñ06ñ11 09.00.00',  'Admin'), ('Vipul', 'Diwan', 200000, '14ñ06ñ11 09.00.00',  'Account'), ('Satish', 'Kumar', 75000, '14ñ01ñ20 09.00.00',  'Account'), ('Geetika', 'Chauhan', 90000, '14ñ04ñ11 09.00.00',  'Admin');
+insert into Employee(First_Name, Last_Name, Salary, Joining_Date, Department) values ('Monika', 'Arora', 100000, '14‚Äì02‚Äì20 09.00.00',  'HR'),('Niharika', 'Verma', 80000, '14‚Äì06‚Äì11 09.00.00',  'Admin'), ('Vishal', 'Singhal', 300000, '14‚Äì02‚Äì20 09.00.00',  'HR'), ('Amitabh', 'Singh', 500000, '14‚Äì02‚Äì20 09.00.00',  'Admin'), ('Vivek', 'Bhati', 500000, '14‚Äì06‚Äì11 09.00.00',  'Admin'), ('Vipul', 'Diwan', 200000, '14‚Äì06‚Äì11 09.00.00',  'Account'), ('Satish', 'Kumar', 75000, '14‚Äì01‚Äì20 09.00.00',  'Account'), ('Geetika', 'Chauhan', 90000, '14‚Äì04‚Äì11 09.00.00',  'Admin');
 drop table Employee;
 create table Bonus(Employee_Ref_ID int, Bonus_Amount int, Bonus_Date varchar(40), Foreign key(Employee_Ref_ID) References Employee(Employee_ID));
 select * from Bonus;
-insert into Bonus(Employee_Ref_ID, Bonus_Amount, Bonus_Date) values (1, 5000, '16ñ02ñ20'), (2, 3000, '16ñ06ñ11'), (3, 4000, '16ñ02ñ20'), (1, 4500, '16ñ02ñ20'), (2, 3500, '16ñ06ñ11');
+insert into Bonus(Employee_Ref_ID, Bonus_Amount, Bonus_Date) values (1, 5000, '16‚Äì02‚Äì20'), (2, 3000, '16‚Äì06‚Äì11'), (3, 4000, '16‚Äì02‚Äì20'), (1, 4500, '16‚Äì02‚Äì20'), (2, 3500, '16‚Äì06‚Äì11');
 create table Title(Employee_Ref_ID int, Employee_Title varchar(30), Affected_From varchar(30) Foreign key(Employee_Ref_ID) References Employee(Employee_ID));
 select * from Title;
-insert into Title(Employee_Ref_ID, Employee_Title, Affected_From) values (1, 'Manager', '2016ñ02ñ20 00:00:00'), (2, 'Executive', '2016ñ06ñ11 00:00:00'), (8, 'Executive', '2016ñ06ñ11 00:00:00'),(5, 'Manager', '2016ñ06ñ11 00:00:00'),(4, 'Asst. Manager', '2016ñ06ñ11 00:00:00'),(7, 'Executive', '2016ñ06ñ11 00:00:00'),(6, 'Lead', '2016ñ06ñ11 00:00:00'),(3, 'Lead', '2016ñ06ñ11 00:00:00');
+insert into Title(Employee_Ref_ID, Employee_Title, Affected_From) values (1, 'Manager', '2016‚Äì02‚Äì20 00:00:00'), (2, 'Executive', '2016‚Äì06‚Äì11 00:00:00'), (8, 'Executive', '2016‚Äì06‚Äì11 00:00:00'),(5, 'Manager', '2016‚Äì06‚Äì11 00:00:00'),(4, 'Asst. Manager', '2016‚Äì06‚Äì11 00:00:00'),(7, 'Executive', '2016‚Äì06‚Äì11 00:00:00'),(6, 'Lead', '2016‚Äì06‚Äì11 00:00:00'),(3, 'Lead', '2016‚Äì06‚Äì11 00:00:00');
 
---Q-1. Write an SQL query to fetch ìFIRST_NAMEî from  Employee table using the alias name as  <EMPLOYEE_NAME>.
+--Q-1. Write an SQL query to fetch ‚ÄúFIRST_NAME‚Äù from  Employee table using the alias name as  <EMPLOYEE_NAME>.
  select First_Name as Employee_Name from Employee;
  
---Q-2. Write an SQL query to fetch ìFIRST_NAMEî from  EMPLOYEE table in upper case. 
+--Q-2. Write an SQL query to fetch ‚ÄúFIRST_NAME‚Äù from  EMPLOYEE table in upper case. 
 select UPPER(First_Name) from Employee;
 
 --Q-3. Write an SQL query to fetch unique values of  DEPARTMENT from EMPLOYEE table. 
 select distinct Department from Employee;
 
---Q-5. Write an SQL query to find the position of the  alphabet (ëaí) in the first name column ëAmitabhí from  EMPLOYEE table. 
---select INSTR(First_Name, Binary 'a') from Employee where First_Name = 'Amitabh';
+--Q-4. Write an SQL query to print the first three characters  of FIRST_NAME from EMPLOYEE table. 
+select SUBSTRING(First_Name,1,3) from Employee;
+
+--Q-5. Write an SQL query to find the position of the  alphabet (‚Äòa‚Äô) in the first name column ‚ÄòAmitabh‚Äô from  EMPLOYEE table. 
+select CHARINDEX('a','Amitabh') as Position;
 
 --Q-6. Write an SQL query to print the FIRST_NAME from  EMPLOYEE table after removing white spaces from the right side. 
 select RTRIM(First_Name) from Employee;
@@ -32,11 +35,11 @@ select LTRIM(Department) from Employee;
 --Q-8. Write an SQL query that fetches the unique values of  DEPARTMENT from EMPLOYEE table and prints its  length. 
 select distinct len(Department) from Employee;
 
---Q-9. Write an SQL query to print the FIRST_NAME from  EMPLOYEE table after replacing ëaí with ëAí. 
+--Q-9. Write an SQL query to print the FIRST_NAME from  EMPLOYEE table after replacing ‚Äòa‚Äô with ‚ÄòA‚Äô. 
 select REPLACE(First_Name, 'a','A') from Employee;
 
 --Q-10. Write an SQL query to print the FIRST_NAME and  LAST_NAME from EMPLOYEE table into a single column COMPLETE_NAME. A space char should separate them.
---select concat(First_Name,'',Last_Name) as Complete_Name from Employee;
+select (First_Name+' '+Last_Name) from Employee;
 
 --Q-11. Write an SQL query to print all EMPLOYEE details  from the EMPLOYEE table order by FIRST_NAME  Ascending. 
 select * from Employee order by First_Name asc;
@@ -44,11 +47,28 @@ select * from Employee order by First_Name asc;
 --Q-12. Write an SQL query to print all EMPLOYEE details  from the EMPLOYEE table order by FIRST_NAME  Ascending and DEPARTMENT Descending. 
 select * from Employee order by Department desc, First_Name asc;
 
---Q-13. Write an SQL query to print details for EMPLOYEEs  with the first name as ìVipulî and ìSatishî from  EMPLOYEE table. 
+--Q-13. Write an SQL query to print details for EMPLOYEEs  with the first name as ‚ÄúVipul‚Äù and ‚ÄúSatish‚Äù from  EMPLOYEE table. 
 select * from Employee where First_Name in ('Vipul','Satish');
 
---Q-14. Write an SQL query to print details of EMPLOYEEs  excluding first names, ìVipulî and ìSatishî from  EMPLOYEE table. 
+--Q-14. Write an SQL query to print details of EMPLOYEEs  excluding first names, ‚ÄúVipul‚Äù and ‚ÄúSatish‚Äù from  EMPLOYEE table. 
 select * from Employee where First_Name not in ('Vipul','Satish');
 
---Q-15. Write an SQL query to print details of EMPLOYEEs  with DEPARTMENT name as ìAdminî. 
+--Q-15. Write an SQL query to print details of EMPLOYEEs  with DEPARTMENT name as ‚ÄúAdmin‚Äù. 
 select * from Employee where Department like 'Admin%';
+
+--Q-16. Write an SQL query to print details of the  EMPLOYEEs whose FIRST_NAME contains ‚Äòa‚Äô.
+select * from Employee where First_Name like '%a%';
+
+--Q-17. Write an SQL query to print details of the  EMPLOYEEs whose FIRST_NAME ends with ‚Äòa‚Äô. 
+select * from Employee where First_Name like '%a';
+
+--Q-18. Write an SQL query to print details of the  EMPLOYEEs whose FIRST_NAME ends with ‚Äòh‚Äô and  contains six alphabets. 
+select * from Employee where First_Name like '_____h';
+
+--Q-19. Write an SQL query to print details of the  EMPLOYEEs whose SALARY lies between 100000 and  500000. 
+select * from Employee where Salary between 100000 and 500000;
+
+--Q-21. Write an SQL query to fetch the count of employees  working in the department ‚ÄòAdmin‚Äô. 
+select COUNT(*) from Employee where Department='Admin';
+
+
